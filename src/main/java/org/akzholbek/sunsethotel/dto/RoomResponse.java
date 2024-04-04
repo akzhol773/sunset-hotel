7c25f98edbd25ebcb8676e22b3b9a465a32d09e4
akzholbek <akzholbek0096@gmail.com>
2024-04-04 10:42:27 +0100

    minor changes

---
+++ src/main/java/org/akzholbek/sunsethotel/dto/RoomResponse.java
@@ -0,0 +1,5 @@
+package org.akzholbek.sunsethotel.dto;
+
+public class RoomResponse {
+
+}
--- src/main/java/org/akzholbek/sunsethotel/model/BookedRoom.java
+++ src/main/java/org/akzholbek/sunsethotel/model/BookedRoom.java
@@ -1,4 +1,67 @@
 package org.akzholbek.sunsethotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numberOfAdults;
+
+    @Column(name = "children")
+    private int numberOfChildren;
+
+    @Column(name = "total_guest")
+    private  int totalNumberOfGuests;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuests(){
+        this.totalNumberOfGuests = this.numberOfAdults + this.numberOfChildren;
+    }
+
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
